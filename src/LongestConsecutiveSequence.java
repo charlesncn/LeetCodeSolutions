844e9fc9cf75e364d2721fe37f701f66f18ccf58
charlesncn <charowcjn@gmail.com>
2022-09-10 11:00:37 +0300

    Implemented LongestConsecutiveSequence

--- src/LongestConsecutiveSequence.java
+++ src/LongestConsecutiveSequence.java
@@ -1,9 +1,34 @@
 public class LongestConsecutiveSequence {
-    public int longestConsecutive(int[] nums){
+    public static int longestConsecutive(int[] nums){
 
+        int max_seq_len = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int current_num = arr[i];
+            int current_seq_len = 1;
+
+            while(numExist(nums, current_num +1)){
+                current_num += 1;
+                current_seq_len += 1;
+            }
+            max_seq_len = Math.max(max_seq_len, current_seq_len);
+
+        }
+        System.out.println(max_seq_len);
+        return max_seq_len;
     }
+
+    private static boolean numExist(int[] arr, int nums) {
+        for (int j : arr) {
+            if (j == nums)
+                return true;
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
 
+        int[] test = {1,2,4,3,6,7,5,23,21,22,23};
+        longestConsecutive(test);
     }
 }
 
